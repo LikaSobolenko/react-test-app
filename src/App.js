e9b14476347f29b86a1d91adaa7b587932e05ee2
Lika Sobolenko <lika160892@yandex.ru>
2023-06-09 17:00:04 +0700

    добавила eslintrc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+
 import "./App.css";
 import Heroes from "./Heroes";
 
 function App() {
-  return (
-    <div className="root">
-      <Heroes />
-    </div>
-  );
+    return (
+        <div className="root">
+            <Heroes />
+        </div>
+    );
 }
 
 export default App;
